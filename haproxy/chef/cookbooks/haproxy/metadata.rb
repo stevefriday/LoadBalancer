90da1c796bb82480877b41ce64496fd8bb61c5cd
Keith Tobin <keith.tobin@gmail.com>
2013-10-10 05:16:53 -0500

    removed haproxy directory form root

--- haproxy/chef/cookbooks/haproxy/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer       "Dell Inc."
-maintainer_email "stevefriday@dell.com"
-license          "All rights reserved"
-description      "haproxy"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
-depends          "oscommon"
--- haproxy/chef/roles/slave.rb
+++
@@ -1,8 +0,0 @@
-name "slave"
-description "Haproxy load-Balancer"
-run_list(
-        "recipe[haproxy::ip_nonlocal_bind]",
-        "recipe[haproxy::install]"
-)
-default_attributes()
-override_attributes()
